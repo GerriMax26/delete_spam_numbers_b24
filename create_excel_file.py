97a99f53505ef715a54bbd682a87ba674089c8f1
Garaev Maksim <garaev.maxim2013@yandex.ru>
2023-03-14 17:10:32 +0300

    add pep8

--- create_excel_file.py
+++ create_excel_file.py
@@ -1,16 +1,19 @@
 import xlsxwriter
 
-def writer_file (function_name):
-    
-    book = xlsxwriter.Workbook(r"C:/Users/admin/Desktop/delete_spam_numbers_b24/spam_contacts.xlsx")
-    page = book.add_worksheet('page_1')
+def writer_file (function_name: list) -> None:
+   """Creates an excel file with phone numbers"""
+   
+   book = xlsxwriter.Workbook(r"C:/Users/admin/Desktop/delete_spam_numbers_b24/spam_contacts.xlsx")
+   page = book.add_worksheet('page_1')
             
-    page.set_column("A:A", 15)
+   page.set_column("A:A", 15)
 
-    row = 0 
-    column = 0
+   row: int = 0 
+   column: int = 0
     
-    for item in function_name:
-       page.write(row, column, item)
-       row+=1
-    book.close()
+   for item in function_name:
+       
+      page.write(row, column, item)
+      row += 1
+   
+   book.close()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from bitrix24 import *
-from create_excel_file import writer_file
-from fast_bitrix24 import Bitrix
 import os
+
+from bitrix24 import *
+from fast_bitrix24 import Bitrix
 from dotenv import load_dotenv
+
+from create_excel_file import writer_file
+
 
 load_dotenv()
 
@@ -10,50 +13,65 @@ bx24 = Bitrix24(os.getenv('bx24'))
 b = Bitrix(os.getenv('b'))
 
 
-
-def create_array_numbers():
+def create_array_numbers() -> list:
+    """Сreates an array of numbers with the name spam to create a excel file"""
     
-    array_all_data_numbers_spam = bx24.callMethod('crm.contact.list',
+    array_all_data_numbers_spam: list = bx24.callMethod('crm.contact.list',
                     filter = { "%NAME": "спам" },
                     select=['NAME', 'PHONE'])
 
-    array_number = [] 
-    array_number_result = []
+    array_number: list = [] 
+    array_number_result: list = []
 
     for i in range(len(array_all_data_numbers_spam)):
+        
         count_element = array_all_data_numbers_spam[i]
         array_number = count_element['PHONE']
+        
         for j in array_number:
             array_number_result.append(j['VALUE'])
 
     for i in range(len(array_number_result)):
         array_number_result[i] = array_number_result[i].replace(' ','')
         array_number_result[i] = array_number_result[i].replace('-','')
+        
     return array_number_result
 
-def create_array_contacts():
+def create_array_contacts() -> list:
+    """Сreates an array of contacts with the name spam"""
     
-    array_all_data_contacts = bx24.callMethod('crm.contact.list',
+    array_all_data_contacts: list = bx24.callMethod('crm.contact.list',
                     filter = { "%NAME": "спам" },
                     select=['NAME', 'PHONE'])
+    
     return array_all_data_contacts
 
 
-def get_id_contacts(array_all_data_contacts):
-    array_id = []
+def get_id_contacts(array_all_data_contacts: list) -> list:
+    """Creates an array of contact id"""
+    
+    array_id: list = []
     
     for id in array_all_data_contacts:
         array_id.append(id['ID'])
     
     return array_id        
 
-def delete_contacts(array_id):
-    dict_id = {}
+def delete_contacts(array_id: list) -> None:
+    """Deletes contacts from crm by their id"""
+    
+    dict_id: dict = {}
     
     for i in array_id:
+        
         dict_id['ID'] = int(i)
         b.call('crm.contact.delete',dict_id)
 
-writer_file(create_array_numbers())
-delete_contacts(get_id_contacts(create_array_contacts()))
+def main() -> None:
+    
+    writer_file(create_array_numbers())
+    delete_contacts(get_id_contacts(create_array_contacts()))
 
+
+if __name__ == '__main__':
+    main()
